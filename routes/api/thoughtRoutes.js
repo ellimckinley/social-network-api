49a39f4a43b3438499aaa90b1e7b2b79a05331eb
ellimckinley <190907228+ellimckinley@users.noreply.github.com>
2025-04-21 08:03:04 -0600

    created thought router

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,11 +1,26 @@
+import { Router } from "express";
+
+const router = Router ();
+import Thought from "../../controllers/thoughtController
+import User from "../../controllers/userController";
+
 // GET for /api/thoughts
+router.route('/').get(getThoughts)
 
 // POST for /api/thoughts
+router.route('/').post(createThought);
 
 // PUT for /api/thoughts
+router.route('/').post(updateThought);
 
 // DELETE for /api/thoughts
+router.route('/').post(deleteThought);
 
 // POST for /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId').post(addReaction);
 
 // DELETE for /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId').delete(removeReaction);
+
+
+export { router as thoughtRouter };
